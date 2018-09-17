d8eef4c0163ee18ee1678faf740118348c82f145
Yuzhi Zhao <zhaoy148@mcmaster.ca>
2018-09-17 10:49:51 -0400

    Create LabelMap and create function to look up labels in the LabelMap according to UIDs.

--- code/drasil-lang/Language/Drasil/Label/Core.hs
+++ code/drasil-lang/Language/Drasil/Label/Core.hs
@@ -2,13 +2,16 @@
 {-# Language FlexibleInstances #-}
 module Language.Drasil.Label.Core where
 
-import Control.Lens (makeLenses)
+import Control.Lens (makeLenses, Lens')
 import Language.Drasil.UID (UID)
 import Language.Drasil.Chunk.ShortName (ShortName, HasShortName(shortname))
 import Language.Drasil.RefTypes (RefType)
+import qualified Data.Map as Map
 
 -- import reference address from Language.Drasil.References?
 data LblType = RefAdd String | MetaLink String | URI String
+
+type LabelMap = Map.Map UID Label
 
 -- Used for referencing; has to be pure ASCII
 data Label = Lbl
@@ -20,6 +23,14 @@ data Label = Lbl
 makeLenses ''Label
 
 instance HasShortName Label where shortname = sn
+
+-- Label Map --
+class HasLabelTable s where
+	labelTable :: Lens' s LabelMap
+
+labelLookup :: UID -> LabelMap -> Label
+labelLookup u l = getL $ Map.lookup u l
+  where getL = maybe (error $ "Label: " ++ u ++ " not found in LabelMap") id
 
 getAdd :: LblType -> String
 getAdd (RefAdd s)   = s
